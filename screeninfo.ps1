25882c4f22b1f56a569ac096487188da020fd72b
AEGIS System <aegis@system.local>
2026-08-17 17:13:08 -0700

    chore: remove debug screenshot artifacts from repo

--- screeninfo.ps1
+++
@@ -1,4 +0,0 @@
-Add-Type -AssemblyName System.Windows.Forms
-foreach ($s in [System.Windows.Forms.Screen]::AllScreens) {
-    Write-Output ("Screen: " + $s.Bounds.Width + "x" + $s.Bounds.Height + " working: " + $s.WorkingArea.Width + "x" + $s.WorkingArea.Height)
-}
